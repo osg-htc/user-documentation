b36f40413db6d674ea382fed91bd213df0ac37a0
Cannon Lock <clock@wisc.edu>
2022-07-08 15:40:00 -0500

    Update the Auto Builder

--- scripts/link-docs.py
+++ scripts/link-docs.py
@@ -5,6 +5,9 @@ import os
 def link_in_documentation():
     """Build the frontmatter for the pages"""
 
+    # Remove the directory first
+    os.system("rm -rf data/docs/osgconnect")
+
     for doc_path in glob.glob("../documentation/**/*.md", recursive=True):
         with open(doc_path, "r") as fp:
             doc = frontmatter.load(fp)
