4b3a0599f3dfecd3a2a6bebbd94ec6b6bbeb9c0e
Cannon Lock <clock@wisc.edu>
2022-07-08 15:59:03 -0500

    Update the relativity of the paths in the action

--- scripts/link-docs.py
+++ scripts/link-docs.py
@@ -8,11 +8,11 @@ def link_in_documentation():
     # Remove the directory first
     os.system("rm -rf data/docs/osgconnect")
 
-    for doc_path in glob.glob("../documentation/**/*.md", recursive=True):
+    for doc_path in glob.glob("documentation/**/*.md", recursive=True):
         with open(doc_path, "r") as fp:
             doc = frontmatter.load(fp)
 
-        destination_path = f"../data/docs/osgconnect/{doc['osgconnect']['path']}"
+        destination_path = f"data/docs/osgconnect/{doc['osgconnect']['path']}"
 
         # Build path if not their
         if not os.path.exists(os.path.dirname(destination_path)):
@@ -20,8 +20,8 @@ def link_in_documentation():
 
         os.symlink(src=os.path.relpath(doc_path, os.path.dirname(destination_path)), dst=destination_path)
 
-    for doc_path in glob.glob("../documentation/assets/**/*.*", recursive=True):
-        destination_path = f"../data/docs/osgconnect/assets/{doc_path.replace('../documentation/assets/', '')}"
+    for doc_path in glob.glob("documentation/assets/**/*.*", recursive=True):
+        destination_path = f"data/docs/osgconnect/assets/{doc_path.replace('documentation/assets/', '')}"
 
         # Build path if not their
         if not os.path.exists(os.path.dirname(destination_path)):
@@ -29,11 +29,11 @@ def link_in_documentation():
 
         os.symlink(src=os.path.relpath(doc_path, os.path.dirname(destination_path)), dst=destination_path)
 
-    for doc_path in glob.glob("../documentation/stylesheets/**/*.css", recursive=True):
+    for doc_path in glob.glob("documentation/stylesheets/**/*.css", recursive=True):
         with open(doc_path, "r") as fp:
             doc = frontmatter.load(fp)
 
-        destination_path = f"../data/docs/osgconnect/{doc['osgconnect']['path']}"
+        destination_path = f"data/docs/osgconnect/{doc['osgconnect']['path']}"
 
         # Build path if not their
         if not os.path.exists(os.path.dirname(destination_path)):
